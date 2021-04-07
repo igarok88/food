7bda197421137fc4ed8c326586c1c8e4fc6b6b6b
igor <usd.wmz@gmail.com>
2021-04-07 12:10:50 +0300

    add comments

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 'use strict';
-
+//срабатывает событие, после того как HTML полностью прогрузился
 window.addEventListener('DOMContentLoaded', () => {
 
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
+	const tabs = document.querySelectorAll('.tabheader__item'), //получаем заголовки табов
+		tabsContent = document.querySelectorAll('.tabcontent'), //получаем контент табов
+		tabsParent = document.querySelector('.tabheader__items'); //получаем родителей заголовков табов
 
 	function hideTabContent() {
-		tabsContent.forEach((item) => {
+		tabsContent.forEach((item) => { //перебираем массив и каждому элементу добавляем класс hide и удаляем add
 			item.classList.add('hide');
 			item.classList.remove('show', 'fade');
 		});
-
+		//убираем класс у заголовков табов
 		tabs.forEach((item) => {
 			item.classList.remove('tabheader__item_active');
 		});
 	}
-
+	//показываем текущий таб, по умолчанию с индексом 0
 	function showTabContent(index = 0) {
 		tabsContent[index].classList.add('show', 'fade');
 		tabsContent[index].classList.remove('hide');
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	hideTabContent();
 	showTabContent();
-
+	//делегируем событие. в родителе табов отслеживаем событие клик, и проверяем если выбраный элемент имеет класс tabheader__item, то начинаем перебор табов и ищем элемент на который мы кликнули, запускаем ф-ции очистки hideTabContent и showTabContent(index) с индексом выбранного элемента
 	tabsParent.addEventListener('click', (event) => {
 		const target = event.target;
 		if (target && target.classList.contains('tabheader__item')) {
